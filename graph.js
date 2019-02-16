05b7ef6d752ea4d0ea9352571be4c7a218daf2b3
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-02-16 12:29:06 -0500

    graph and svg append to the canvas

--- graph.js
+++ graph.js
@@ -1,3 +1,11 @@
+const dims = { height: 500, width: 110 }
+const svg = d3.select('.canvas')
+    .append('svg')
+    .attr('width', dims.width + 100)
+    .attr('height', dims.height + 100);
+const graph = svg.append('g')
+    .attr('transform', 'translate(50,50)');
+
 // data and firebase hook up
 var data = [];
 
